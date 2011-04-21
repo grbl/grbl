6ed9c16cb76d75cf5cf32db48b0ec1b453d0a49d
aldenhart <alden08@harts.org>
2011-04-21 10:46:45 -0400

    updating grblshield patches

---
+++ limits.h
@@ -0,0 +1,30 @@
+/*
+  limits.h - code pertaining to limit-switches and performing the homing cycle
+  Part of Grbl
+
+  Copyright (c) 2009-2011 Simen Svale Skogsrud
+
+  Grbl is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+
+  Grbl is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with Grbl.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#ifndef limits_h
+#define limits_h 
+
+// initialize the limits module
+void limits_init();
+
+// perform the homing cycle
+void limits_go_home();
+
+#endif
--- protocol.c
+++ protocol.c
@@ -1,5 +1,5 @@
 /*
-  serial_protocol.c - the serial protocol master control unit
+  protocol.c - the serial protocol master control unit
   Part of Grbl
 
   Copyright (c) 2009-2011 Simen Svale Skogsrud
@@ -19,7 +19,7 @@
 */
 
 #include <avr/io.h>
-#include "serial_protocol.h"
+#include "protocol.h"
 #include "gcode.h"
 #include "wiring_serial.h"
 #include "settings.h"
@@ -32,40 +32,51 @@
 static char line[LINE_BUFFER_SIZE];
 static uint8_t char_counter;
 
-void status_message(int status_code) {
-  switch(status_code) {          
-    case GCSTATUS_OK:
-    printPgmString(PSTR("ok\n\r")); break;
-    case GCSTATUS_BAD_NUMBER_FORMAT:
-    printPgmString(PSTR("error: Bad number format\n\r")); break;
-    case GCSTATUS_EXPECTED_COMMAND_LETTER:
-    printPgmString(PSTR("error: Expected command letter\n\r")); break;
-    case GCSTATUS_UNSUPPORTED_STATEMENT:
-    printPgmString(PSTR("error: Unsupported statement\n\r")); break;
-    case GCSTATUS_FLOATING_POINT_ERROR:
-    printPgmString(PSTR("error: Floating point error\n\r")); break;
-    default:
+static void status_message(int status_code) {
+  if (status_code == 0) {
+    printPgmString(PSTR("ok\r\n"));
+  } else {
     printPgmString(PSTR("error: "));
-    printInteger(status_code);
-    printPgmString(PSTR("\n\r"));
+    switch(status_code) {          
+      case STATUS_BAD_NUMBER_FORMAT:
+      printPgmString(PSTR("Bad number format\r\n")); break;
+      case STATUS_EXPECTED_COMMAND_LETTER:
+      printPgmString(PSTR("Expected command letter\r\n")); break;
+      case STATUS_UNSUPPORTED_STATEMENT:
+      printPgmString(PSTR("Unsupported statement\r\n")); break;
+      case STATUS_FLOATING_POINT_ERROR:
+      printPgmString(PSTR("Floating point error\r\n")); break;
+      default:
+      printInteger(status_code);
+      printPgmString(PSTR("\r\n"));
+    }
   }
 }
 
-void sp_init() 
+void protocol_init() 
 {
   beginSerial(BAUD_RATE);  
   printPgmString(PSTR("\r\nGrbl " GRBL_VERSION));
   printPgmString(PSTR("\r\n"));  
 }
 
-void sp_process()
+// Executes one line of input according to protocol
+uint8_t protocol_execute_line(char *line) {
+  if(line[0] == '$') {
+    return(settings_execute_line(line)); // Delegate lines starting with '$' to the settings module
+  } else {
+    return(gc_execute_line(line));    // Everything else is gcode
+  }
+}
+
+void protocol_process()
 {
   char c;
   while((c = serialRead()) != -1) 
   {
     if((char_counter > 0) && ((c == '\n') || (c == '\r'))) {  // Line is complete. Then execute!
       line[char_counter] = 0; // treminate string
-      status_message(gc_execute_line(line));
+      status_message(protocol_execute_line(line));
       char_counter = 0; // reset line buffer index
     } else if (c <= ' ') { // Throw away whitepace and control characters
     } else if (c >= 'a' && c <= 'z') { // Upcase lowercase
