5ecc661ec8adc61a9e4f9dec704f259450309394
Bertus Kruger <krugercreations@gmail.com>
2013-03-13 09:44:22 +1300

    Update eeprom.h
    Fixed the function signature.

--- eeprom.h
+++ eeprom.h
@@ -2,7 +2,7 @@
 #define eeprom_h
 
 char eeprom_get_char(unsigned int addr);
-void eeprom_put_char(unsigned int addr, char new_value);
+void eeprom_put_char( unsigned int addr, unsigned char new_value );
 void memcpy_to_eeprom_with_checksum(unsigned int destination, char *source, unsigned int size);
 int memcpy_from_eeprom_with_checksum(char *destination, unsigned int source, unsigned int size);
 
