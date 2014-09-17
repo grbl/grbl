ff139b0742f3c3599a6203aa63ec869841ddde32
Markus Schulz <schulz@alpharesearch.de>
2014-09-17 19:17:02 -0400

    try min max spindle from 1 to 256, it's only 8 bit anyway

--- config.h
+++ config.h
@@ -161,8 +161,8 @@
 // equally divided voltage bins between the maximum and minimum spindle speeds. So for a 5V pin, 1000
 // max rpm, and 250 min rpm, the spindle output voltage would be set for the following "S" commands: 
 // "S1000" @ 5V, "S250" @ 0.02V, and "S625" @ 2.5V (mid-range). The pin outputs 0V when disabled.
-#define SPINDLE_MAX_RPM 1000.0 // Max spindle RPM. This value is equal to 100% duty cycle on the PWM.
-#define SPINDLE_MIN_RPM 0.0    // Min spindle RPM. This value is equal to (1/256) duty cycle on the PWM.
+#define SPINDLE_MAX_RPM 256.0 // Max spindle RPM. This value is equal to 100% duty cycle on the PWM.
+#define SPINDLE_MIN_RPM 1.0    // Min spindle RPM. This value is equal to (1/256) duty cycle on the PWM.
 
 // Minimum planner junction speed. Sets the default minimum junction speed the planner plans to at
 // every buffer block junction, except for starting from rest and end of the buffer, which are always
