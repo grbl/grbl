c1a469bbbe8a4573f039c8f008349fc0b934da4c
Markus Schulz <schulz@alpharesearch.de>
2014-09-17 21:48:59 -0400

    add laser spindle define to turn dwelling off.

--- config.h
+++ config.h
@@ -164,6 +164,10 @@
 #define SPINDLE_MAX_RPM 256.0 // Max spindle RPM. This value is equal to 100% duty cycle on the PWM.
 #define SPINDLE_MIN_RPM 1.0    // Min spindle RPM. This value is equal to (1/256) duty cycle on the PWM.
 
+// This need variable spinle enabaled. Lasers don't need time to speed up or slow down and the build 
+// in dwelling for spindels will be disabled. 
+#define LASER_SPINDLE // Default disabled. Uncomment to enable.
+
 // Minimum planner junction speed. Sets the default minimum junction speed the planner plans to at
 // every buffer block junction, except for starting from rest and end of the buffer, which are always
 // zero. This value controls how fast the machine moves through junctions with no regard for acceleration
