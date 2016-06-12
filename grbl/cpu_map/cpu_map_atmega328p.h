bf2e23679eca18b35f1e6e8e1f45b9e737340f9f
ruizivo <ruizivo.menezes@gmail.com>
2016-06-12 15:58:48 -0300

    alter pins to unipolar motors

--- grbl/cpu_map/cpu_map_atmega328p.h
+++ grbl/cpu_map/cpu_map_atmega328p.h
@@ -36,23 +36,23 @@
 // Define step pulse output pins. NOTE: All step bit pins must be on the same port.
 #define STEP_DDR        DDRD
 #define STEP_PORT       PORTD
-#define X_STEP_BIT      2  // Uno Digital Pin 2
-#define Y_STEP_BIT      3  // Uno Digital Pin 3
-#define Z_STEP_BIT      4  // Uno Digital Pin 4
+#define X_STEP_BIT      0//2  // Uno Digital Pin 2
+#define Y_STEP_BIT      0//3  // Uno Digital Pin 3
+#define Z_STEP_BIT      0//4  // Uno Digital Pin 4
 #define STEP_MASK       ((1<<X_STEP_BIT)|(1<<Y_STEP_BIT)|(1<<Z_STEP_BIT)) // All step bits
 
 // Define step direction output pins. NOTE: All direction pins must be on the same port.
 #define DIRECTION_DDR     DDRD
 #define DIRECTION_PORT    PORTD
-#define X_DIRECTION_BIT   5  // Uno Digital Pin 5
-#define Y_DIRECTION_BIT   6  // Uno Digital Pin 6
-#define Z_DIRECTION_BIT   7  // Uno Digital Pin 7
+#define X_DIRECTION_BIT   6//5  // Uno Digital Pin 5
+#define Y_DIRECTION_BIT   7//6  // Uno Digital Pin 6
+#define Z_DIRECTION_BIT   0//7  // Uno Digital Pin 7
 #define DIRECTION_MASK    ((1<<X_DIRECTION_BIT)|(1<<Y_DIRECTION_BIT)|(1<<Z_DIRECTION_BIT)) // All direction bits
 
 // Define stepper driver enable/disable output pin.
 #define STEPPERS_DISABLE_DDR    DDRB
 #define STEPPERS_DISABLE_PORT   PORTB
-#define STEPPERS_DISABLE_BIT    0  // Uno Digital Pin 8
+#define STEPPERS_DISABLE_BIT    5//0  // Uno Digital Pin 8
 #define STEPPERS_DISABLE_MASK   (1<<STEPPERS_DISABLE_BIT)
 
 // Define homing/hard limit switch input pins and limit interrupt vectors. 
@@ -109,10 +109,10 @@
 #define CONTROL_DDR       DDRC
 #define CONTROL_PIN       PINC
 #define CONTROL_PORT      PORTC
-#define RESET_BIT         0  // Uno Analog Pin 0
-#define FEED_HOLD_BIT     1  // Uno Analog Pin 1
-#define CYCLE_START_BIT   2  // Uno Analog Pin 2
-#define SAFETY_DOOR_BIT   1  // Uno Analog Pin 1 NOTE: Safety door is shared with feed hold. Enabled by config define.
+#define RESET_BIT         6//0  // Uno Analog Pin 0
+#define FEED_HOLD_BIT     6//1  // Uno Analog Pin 1
+#define CYCLE_START_BIT   6//2  // Uno Analog Pin 2
+#define SAFETY_DOOR_BIT   6//1  // Uno Analog Pin 1 NOTE: Safety door is shared with feed hold. Enabled by config define.
 #define CONTROL_INT       PCIE1  // Pin change interrupt enable pin
 #define CONTROL_INT_vect  PCINT1_vect
 #define CONTROL_PCMSK     PCMSK1 // Pin change interrupt register
