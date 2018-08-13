df87b36f48b90930714accf03b411b60d5d948e5
Sonny Jeon <chamnit@gmail.com>
2018-08-13 15:03:14 -0600

    Updated version.

--- grbl/grbl.h
+++ grbl/grbl.h
@@ -22,7 +22,7 @@
 #define grbl_h
 
 // Grbl versioning system
-#define GRBL_VERSION "1.1f"
+#define GRBL_VERSION "1.1g"
 #define GRBL_VERSION_BUILD "20180813.Mega"
 
 // Define standard libraries used by Grbl.
